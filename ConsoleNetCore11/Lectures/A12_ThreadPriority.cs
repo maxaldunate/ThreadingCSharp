0f615e43c6101f0c36609983ce17a1236c6163e4
maxaldunate <maxaldunate@gmail.com>
2017-05-04 16:51:12 +0200

    real time

---
+++ ConsoleNetCore11/Lectures/A12_ThreadPriority.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using System.Threading;
+
+namespace ConsoleNetCore11.Lectures
+{
+    public class A12_ThreadPriority
+    {
+        enum ThreadPriority { Lowest, BelowNormal, Normal, AboveNormal, Highest }
+
+        public static void Run()
+        {
+            using (Process p = Process.GetCurrentProcess())
+                p.PriorityClass = ProcessPriorityClass.High;
+            //ProcessPriorityClass.RealTime
+        }
+    }
+}
