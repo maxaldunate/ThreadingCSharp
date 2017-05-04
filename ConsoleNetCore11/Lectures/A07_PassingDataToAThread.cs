51b1519d50582949c1977b813757fcd6d4118aba
maxaldunate <maxaldunate@gmail.com>
2017-05-04 10:14:26 +0200

    add gitignore, and passing dato to a thread

--- ConsoleNetCore11/Lectures/A07_PassingDataToAThread.cs
+++ ConsoleNetCore11/Lectures/A07_PassingDataToAThread.cs
@@ -8,13 +8,31 @@ namespace ConsoleNetCore11.Lectures
 
         public static void Run()
         {
-            Console.WriteLine("Thread ended!");
+            //Using multi lambda
+            new Thread(() =>
+            {
+                Console.WriteLine("I'm running on another thread!");
+                Console.WriteLine("This is so easy!");
+            }).Start();
+
+            //Sending a string
+            Thread t = new Thread(() => Print("Hello from string!"));
+            t.Start();
+
+            //Sending an object
+            Thread t2 = new Thread(PrintObj);
+            t2.Start("Hello from object!");
         }
 
-        public static void Go()
+        public static void Print(string message)
         {
-            Console.WriteLine("hello!");
+            Console.WriteLine(message);
         }
 
+        public static void PrintObj(object messageObj)
+        {
+            string message = (string)messageObj;   // We need to cast here
+            Console.WriteLine(message);
+        }
     }
 }
